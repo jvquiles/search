4b9cb20144331c379595809ebb79cde30f8ce9a0
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-05 16:17:28 +0200

    test: Add empty criteria test case

---
+++ Search.Tests/CitySearchShould.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Search.Tests
+{
+    public class CitySearchShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FindEmptyCriteria()
+        {
+            var foundCities = CitySearch.Find("");
+            foundCities.Should().BeEmpty();
+        }
+    }
+
+    public class CitySearch
+    {
+        private List<string> _cities = new List<string>(){ "Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul" };
+
+        public static List<string> Find(string criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
